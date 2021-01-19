006ab8a910ac9f298f5624645a2e6010aa68f8e9
Viraj Mehta <thebigmehtaphor@gmail.com>
2021-01-19 15:28:44 -0600

    wip

--- bats.py
+++ bats.py
@@ -5,11 +5,16 @@ class BATSTrainer:
     def __init__(self, dataset, env, **kwargs):
         self.dataset = dataset
         self.env = env
+        self.gamma = kwargs.get('gamma', 0.99)
+        self.dataset_size = dataset['observations'].shape[0]
+        # set up graph
         self.G = Graph()
+        # add a vertex for each state
+        self.G.add_vertex(self.dataset_size)
 
     def train(self):
-        self.train_dynamics()
         self.add_dataset_edges()
+        self.train_dynamics()
         possible_neighbors = self.find_possible_neighbors()
         self.add_neighbor_edges(possible_neighbors)
         self.value_iteration()
