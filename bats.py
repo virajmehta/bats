f727b863a023701b6083d23ab5b23912376f40ed
Viraj Mehta <thebigmehtaphor@gmail.com>
2021-01-19 20:55:32 -0600

    added possible neighbor filtering

--- bats.py
+++ bats.py
@@ -10,22 +10,24 @@ class BATSTrainer:
         self.gamma = kwargs.get('gamma', 0.99)
         self.tqdm = kwargs.get('tqdm', True)
         all_obs = np.concatenate((dataset['observations'], dataset['next_observations']))
-        unique_obs = np.unique(all_obs, axis=0)
-        self.graph_size = unique_obs.shape[0]
+        self.unique_obs = np.unique(all_obs, axis=0)
+        self.graph_size = self.unique_obs.shape[0]
         self.dataset_size = self.dataset['observations'].shape[0]
+        # could do it this way or with knn, this is simpler to implement for now
+        self.epsilon_neighbors = kwargs.get('epsilon_neighors', 0.1)
         # set up graph
         self.G = Graph()
         # add a vertex for each state
         self.G.add_vertex(self.graph_size)
         # make sure we keep the mapping from states to vertices
-        self.vertices = {obs.tobytes(): i for i, obs in enumerate(unique_obs)}
+        self.vertices = {obs.tobytes(): i for i, obs in enumerate(self.unique_obs)}
         # the values associated with each node
-        self.v_value = self.G.new_vertex_property("float")
-        self.v_value.get_array()[:] = 0
+        self.G.vp.value = self.G.new_vertex_property("float")
+        self.G.vp.get_array()[:] = 0
         # the actions are gonna be associated with each edge
-        self.e_action = self.G.new_edge_property("vector<float>")
+        self.G.ep.action = self.G.new_edge_property("vector<float>")
         # we also associate the rewards with each edge
-        self.e_reward = self.G.new_edge_property("float")
+        self.G.ep.reward = self.G.new_edge_property("float")
 
     def get_vertex(self, obs):
         return self.G.vertex(self.vertices[obs.tobytes()])
@@ -63,7 +65,12 @@ class BATSTrainer:
             self.e_reward[e] = reward
 
     def find_possible_neighbors(self):
-        raise NotImplementedError()
+        pairwise_distances = np.sqrt(np.sum((self.unique_obs[None, :] - self.unique_obs[:, None]) ** 2, axis=-1))
+        possible_neighbors = pairwise_distances < self.epsilon_neighbors
+        possible_neighbors_list = np.argwhere(possible_neighbors)
+        nonduplicates = possible_neighbors_list[:, 0] != possible_neighbors_list[:, 1]
+        nonduplicate_possible_neighbors_list = np.compress(nonduplicates, possible_neighbors_list, axis=0)
+        return nonduplicate_possible_neighbors_list
 
     def value_iteration(self):
         raise NotImplementedError()
