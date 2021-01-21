c3a5168c4c269ad043b45cd021a72f92d26513ec
Viraj Mehta <thebigmehtaphor@gmail.com>
2021-01-21 11:35:57 -0600

    everything runs!

--- bats.py
+++ bats.py
@@ -1,11 +1,11 @@
 from graph_tool import Graph
 import util
+import time
 import numpy as np
 from tqdm import trange, tqdm
 from modelling.dynamics_construction import train_ensemble
 from modelling.policy_construction import train_policy
 from sklearn.neighbors import radius_neighbors_graph
-from ipdb import set_trace as db  # NOQA
 
 
 class BATSTrainer:
@@ -26,19 +26,19 @@ class BATSTrainer:
         self.dynamics_train_params['n_members'] = kwargs.get('dynamics_n_members', 7)
         self.dynamics_train_params['n_elites'] = kwargs.get('dynamics_n_elites', 5)
         self.dynamics_train_params['save_dir'] = str(output_dir)
-        self.dynamics_train_params['epochs'] = kwargs.get('dynamics_epochs', 2)
+        self.dynamics_train_params['epochs'] = kwargs.get('dynamics_epochs', 100)
         self.dynamics_train_params['cuda_device'] = kwargs.get('cuda_device', '')
 
         # set up the parameters for behavior cloning
         self.policy = None
         self.bc_params = {}
         self.bc_params['save_dir'] = str(output_dir)
-        self.bc_params['epochs'] = kwargs.get('bc_epochs', 2)
+        self.bc_params['epochs'] = kwargs.get('bc_epochs', 100)
         self.bc_params['cuda_device'] = kwargs.get('cuda_device', '')
         # self.bc_params['hidden_sizes'] = kwargs.get('policy_hidden_sizes', '256, 256')
 
         # could do it this way or with knn, this is simpler to implement for now
-        self.epsilon_neighbors = kwargs.get('epsilon_neighors', 2)
+        self.epsilon_neighbors = kwargs.get('epsilon_neighors', 2)  # no idea what this should be
         # set up graph
         self.G = Graph()
         # add a vertex for each state
@@ -54,6 +54,10 @@ class BATSTrainer:
         # we also associate the rewards with each edge
         self.G.ep.reward = self.G.new_edge_property("float")
 
+        # parameters for planning
+        self.epsilon_planning = kwargs.get('epsilon_planning', 0.1)  # also no idea what this should be
+        self.planning_quantile = kwargs.get('planning_quantile', 0.8)
+
         # parameters for evaluation
         self.num_eval_episodes = kwargs.get("num_eval_episodes", 20)
 
@@ -68,7 +72,9 @@ class BATSTrainer:
 
     def train(self):
         self.add_dataset_edges()
+        start = time.time()
         possible_neighbors = self.find_possible_neighbors()
+        print(f"Time: {time.time() - start}")
         self.train_dynamics()
         self.add_neighbor_edges(possible_neighbors)
         self.value_iteration()
@@ -82,23 +88,24 @@ class BATSTrainer:
     def add_neighbor_edges(self, possible_neighbors):
         '''
         '''
+        print('testing possible neighbors')
         edges_to_add = []
         for row in possible_neighbors:
             adding_neighbor = row[0]
             receiving_neighbor = row[1]
             receiving_obs = self.unique_obs[receiving_neighbor, :]
-            add_vertex = self.G.vertex(adding_neighbor)
-            for edge in add_vertex.iter_out_edges():
-                action = np.array(self.G.ep.action[edge])
-                target_vertex = edge.target()
-                obs_index = self.G.vertex_index[target_vertex]
-                target_obs = self.unique_obs[obs_index, :]
-                # need to run MPC to go from recieving_obs to target_obs with action initialized at action
+            for start, end, action in self.G.iter_out_edges(adding_neighbor, eprops=[self.G.ep.action]):
+                target_obs = self.unique_obs[end, :]
+                # need to run CEM to go from recieving_obs to target_obs with action initialized at action
                 # could also add kwargs to adjust the CEM parameters like max_iters, popsize, etc.
-                best_action, predicted_reward = util.CEM(receiving_obs, target_obs, action, self.dynamics_ensemble,
-                                                         self.epsilon_planning, self.planning_quantile)
+                best_action, predicted_reward = util.CEM(receiving_obs,
+                                                         target_obs,
+                                                         np.array(action),
+                                                         self.dynamics_ensemble,
+                                                         self.epsilon_planning,
+                                                         self.planning_quantile)
                 if best_action is not None:
-                    edges_to_add.append((receiving_neighbor, obs_index, best_action, predicted_reward))
+                    edges_to_add.append((receiving_neighbor, end, best_action, predicted_reward))
         print(f"adding {len(edges_to_add)} edges to graph")
         for start, end, action, reward in edges_to_add:
             e = self.G.add_edge(start, end)
@@ -122,17 +129,26 @@ class BATSTrainer:
     def find_possible_neighbors(self):
         print("finding possible neighbors")
         neighbors = radius_neighbors_graph(self.unique_obs, self.epsilon_neighbors, n_jobs=-1)
-        return np.column_stack(neighbors.nonzero())
+        possible_neighbors = np.column_stack(neighbors.nonzero())
+        print(f"found {possible_neighbors.shape[0] // 2} possible neighbors")
+        return possible_neighbors
 
     def value_iteration(self):
+        print("performing value iteration")
         iterator = self.get_iterator(self.n_val_iterations)
         for _ in iterator:
             for v in self.G.iter_vertices():
                 # should be a (num_neighbors, 2) ndarray where the first col is indices and second is values
                 neighbors = self.G.get_out_neighbors(v, vprops=[self.G.vp.value])
-                edges = self.G.get_out_edges(v, eprops=[self.G.ep.reward])
-                rewards = edges[:, 1]
+                if len(neighbors) == 0:
+                    # the state has no actions in our graph
+                    self.G.vp.value[v] = 0
+                    self.G.vp.best_neighbor[v] = -1
+                    continue
                 values = neighbors[:, 1]
+                # should be a (num_neighbors, 3) ndarray where the first col is v second col is indices, third is reward
+                edges = self.G.get_out_edges(v, eprops=[self.G.ep.reward])
+                rewards = edges[:, 2]
                 backups = rewards + self.gamma * values
                 best_arm = np.argmax(backups)
                 value = backups[best_arm]
@@ -140,14 +156,21 @@ class BATSTrainer:
                 self.G.vp.best_neighbor[v] = neighbors[best_arm, 0]
 
     def train_bc(self):
+        print("cloning a policy")
         actions = []
+        bad_indices = []
         for v in self.G.iter_vertices():
-            best_neighbor = self.G.best_neighbor[v]
+            best_neighbor = self.G.vp.best_neighbor[v]
+            if best_neighbor == -1:
+                # there were no outgoing edges found from this vertex
+                bad_indices.append(v)
+                continue
             e = self.G.edge(v, best_neighbor)
             action = np.array(self.G.ep.action[e])
             actions.append(action)
         actions = np.stack(actions)
-        dataset = {'observations': self.unique_obs,
+        obs = np.delete(self.unique_obs, bad_indices, axis=0)
+        dataset = {'observations': obs,
                    'actions': actions}
         self.policy = train_policy(dataset, **self.bc_params)
 
