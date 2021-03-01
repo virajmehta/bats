4add77290c21faccfafda7f5c2e351019582f0be
IanChar <ianchar4@gmail.com>
2021-03-01 09:40:44 -0500

    Added ability estimate maze value.

--- scripts/estimate_returns_from_graph.py
+++ scripts/estimate_returns_from_graph.py
@@ -3,16 +3,25 @@ Estimate what the policy's value will be based on the graph.
 """
 import argparse
 
+import d4rl
+import gym
 import h5py
 import numpy as np
 
 from graph_tool import load_graph
 from modelling.utils.graph_util import get_best_policy_returns
+from examples.mazes.maze_util import get_starts_from_graph
 
 
 def run(args):
     graph = load_graph(args.graph_path)
-    returns = get_best_policy_returns(graph, horizon=args.horizon)
+    if args.env is not None and 'maze' in args.env:
+        env = gym.make(args.env)
+        starts = get_starts_from_graph(graph, env)
+    else:
+        starts = None
+    returns = get_best_policy_returns(graph, starts=starts,
+                                      horizon=args.horizon)
     rets = np.array([r[0] for r in returns]).reshape(-1, 1)
     obs = np.vstack([r[1] for r in returns])
     ts = np.array([r[2] for r in returns]).reshape(-1, 1)
@@ -27,6 +36,7 @@ def run(args):
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--graph_path', required=True)
+    parser.add_argument('--env')
     parser.add_argument('--save_path')
     parser.add_argument('--horizon', type=int, default=1000)
     parser.add_argument('--pudb', action='store_true')
