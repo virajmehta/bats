0dffc9f5d260e08b08a8c6881586cf81ec324727
Viraj Mehta <virajm@cs.cmu.edu>
2021-03-17 14:46:54 -0700

    added load_bisimulation function

--- modelling/bisim_construction.py
+++ modelling/bisim_construction.py
@@ -6,12 +6,11 @@ from collections import OrderedDict
 from copy import deepcopy
 import pickle as pkl
 
-import numpy as np
 from sklearn.model_selection import train_test_split
 import torch
 from torch.utils.data import DataLoader, TensorDataset
 
-from modelling.dynamics_construction import get_pnn_optimizer
+# from modelling.dynamics_construction import get_pnn_optimizer
 from modelling.models import BisimulationModel
 from modelling.trainers import ModelTrainer
 from modelling.utils.torch_utils import swish
@@ -76,7 +75,7 @@ def train_bisim(
         save_path=save_dir,
         silent=silent,
         optimizers=optimizers,
-        validation_tune_metric='EncoderLoss', # TODO: Make this better.
+        validation_tune_metric='EncoderLoss',  # TODO: Make this better.
         track_stats=['EncoderLoss', 'PNN1_Loss'],
     )
     with open(os.path.join(save_dir, 'params.pkl'), 'wb') as f:
@@ -173,3 +172,16 @@ def get_tr_val_data(
         pin_memory=use_gpu,
     )
     return tr_data, val_data
+
+
+def load_bisimulation(
+        load_dir,
+        obs_dim,
+        act_dim,
+        latent_dim
+        ):
+    with open(load_dir / 'params.pkl', 'rb') as f:
+        params = pkl.load(f)
+    model = get_bisim(obs_dim, act_dim, latent_dim, **params)
+    model.load_model(load_dir / 'model.pt')
+    return model
