27cace5abb98b04d305e22d16d52409a937664f2
Viraj Mehta <virajm@cs.cmu.edu>
2021-04-09 16:07:20 -0400

    wip

--- hp_tuning.py
+++ hp_tuning.py
@@ -1,6 +1,9 @@
 from run import main, parse_arguments
+import numpy as np
 from functools import partial
 from copy import deepcopy
+
+NUM_TRIALS = 8
 
 
 def training_function(config, args):
@@ -16,7 +19,21 @@ def training_function(config, args):
 
 def hp_main(args):
     train_fn = partial(training_function, args=args)
-    raise NotImplementedError()
+    # for now just doing a uniform distribution over these
+    best_return = -np.inf
+    best_config = None
+    config_space = {
+            'epsilon_planning': (0.05, 0.5),
+            'planning_quantile': (0.4, 1.),
+            'epsilon_neighbors': (1, 5),
+            }
+    for i in trange(NUM_TRIALS):
+        config = sample_config(config_space)
+        returns = train_fn(config)
+        best_iter = np.argmax(returns)
+        current_best_return = returns[best_iter]
+        if current_best_return > best_return:
+            best_return = current
 
 
 if __name__ == '__main__':
