28a6b056616b4405e5a5f9f89d9826ef91fa4e5c
Viraj Mehta <mehtav@br013.ib.bridges2.psc.edu>
2021-05-12 14:53:07 -0400

    fixed hopper config

--- configs/hopper.py
+++ configs/hopper.py
@@ -7,27 +7,47 @@ from copy import deepcopy
 base_config = OrderedDict(
     epsilon_planning=0.07,
     epsilon_neighbors=0.3,
-    num_cpus=84,
+    num_cpus=60,
     stitching_chunk_size=50000,
+    normalize_obs=True,
 )
 
 # For any additional configurations, add them here.
 HOPPER_CONFIGS = OrderedDict()
 
 HOPPER_CONFIGS['hopper-expert'] = deepcopy(base_config)
-HOPPER_CONFIGS['hopper-expert']['env_name'] = 'hopper-expert-v2'
+HOPPER_CONFIGS['hopper-expert']['env_name'] = 'hopper-expert-v0'
 
-HOPPER_CONFIGS['hopper-medium-expert'] = deepcopy(base_config)
-HOPPER_CONFIGS['hopper-medium-expert']['env_name'] =\
-    'hopper-medium-expert-v2'
+HOPPER_CONFIGS['hopper-medexp'] = deepcopy(base_config)
+HOPPER_CONFIGS['hopper-medexp']['env_name'] =\
+    'hopper-medium-expert-v0'
 
 HOPPER_CONFIGS['hopper-random'] = deepcopy(base_config)
-HOPPER_CONFIGS['hopper-random']['env_name'] = 'hopper-random-v2'
+HOPPER_CONFIGS['hopper-random']['env_name'] = 'hopper-random-v0'
 
 HOPPER_CONFIGS['hopper-mixed'] = deepcopy(base_config)
 HOPPER_CONFIGS['hopper-mixed']['env_name'] =\
-    'hopper-medium-replay-v2'
+    'hopper-medium-replay-v0'
 HOPPER_CONFIGS['hopper-mixed']['load_model'] = 'experiments/hopper_medium_replay_bolt'
 
 HOPPER_CONFIGS['hopper-medium'] = deepcopy(base_config)
-HOPPER_CONFIGS['hopper-medium']['env_name'] = 'hopper-medium-v2'
+HOPPER_CONFIGS['hopper-medium']['env_name'] = 'hopper-medium-v0'
+
+to_add = OrderedDict()
+for k, v in HOPPER_CONFIGS.items():
+    task_type = k[k.index('-') + 1:]
+    config = deepcopy(v)
+    config['use_all_planning_itrs'] = True
+    config['continue_after_no_advantage'] = True
+    config['num_stitching_iters'] = 25
+    # For mixed dataset edge distance = 0.726 +- 0.632
+    # config['epsilon_neighbors'] = 0.3
+    config['planning_quantile'] = 0.4
+    config['epsilon_planning'] = 5
+    config['load_model'] = ('/zfsauton/project/public/ichar/'
+                            'd4rl_models/hopper/hp_%s' % task_type)
+    config['verbose'] = True
+    config['k_neighbors'] = 25
+    config['max_stitch_length'] = 5
+    to_add[k + '-tune'] = config
+HOPPER_CONFIGS.update(to_add)
