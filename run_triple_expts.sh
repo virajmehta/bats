f66c581e3f14561fdae558fe72aeba1f1cf54a2c
Viraj Mehta <virajm@cs.cmu.edu>
2021-05-16 13:46:42 -0700

    fixed triple run script

--- run_triple_expts.sh
+++ run_triple_expts.sh
@@ -1,6 +1,9 @@
 all_args=("$@")
 first_arg=$1
-rest_args=("${all_args[@]:2}")
+rest_args=("${all_args[@]:1}")
+echo "sbatch psc_run.sbatch ${first_arg}_s0" "${rest_args[@]}"
 sbatch psc_run.sbatch "${first_arg}_s0" "${rest_args[@]}"
+echo "sbatch psc_run.sbatch ${first_arg}_s1" "${rest_args[@]}"
 sbatch psc_run.sbatch "${first_arg}_s1" "${rest_args[@]}"
+echo "sbatch psc_run.sbatch ${first_arg}_s2" "${rest_args[@]}"
 sbatch psc_run.sbatch "${first_arg}_s2" "${rest_args[@]}"
