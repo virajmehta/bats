e0b35e74e540048e29239e66d7aff0b2d03373b6
Viraj Mehta <virajm@cs.cmu.edu>
2021-05-16 20:28:35 -0700

    added prod configs for hc hopper walker

--- configs/halfcheetah.py
+++ configs/halfcheetah.py
@@ -8,10 +8,10 @@ from pathlib import Path
 base_config = OrderedDict(
     epsilon_planning=0.4,
     epsilon_neighbors=1.3,
-    num_cpus=60,
+    num_cpus=40,
     stitching_chunk_size=50000,
     normalize_obs=True,
-    num_stitching_iterations=20,
+    num_stitching_iters=20,
 )
 
 # For any additional configurations, add them here.
@@ -30,11 +30,10 @@ HALFCHEETAH_CONFIGS['halfcheetah-random']['env_name'] = 'halfcheetah-random-v2'
 HALFCHEETAH_CONFIGS['halfcheetah-mixed'] = deepcopy(base_config)
 HALFCHEETAH_CONFIGS['halfcheetah-mixed']['env_name'] =\
     'halfcheetah-medium-replay-v2'
-HALFCHEETAH_CONFIGS['halfcheetah-mixed']['load_bisim_model'] = Path("/zfsauton/project/public/ichar/models/bisimulation/halfcheetah")  # NOQA
-HALFCHEETAH_CONFIGS['halfcheetah-mixed']['use_bisimulation'] = True
-HALFCHEETAH_CONFIGS['halfcheetah-mixed']['penalize_stitches'] = True
-HALFCHEETAH_CONFIGS['halfcheetah-mixed']['num_stitching_iterations'] = 10
-HALFCHEETAH_CONFIGS['halfcheetah-mixed']['bc_every_iter'] = True
+# HALFCHEETAH_CONFIGS['halfcheetah-mixed']['load_bisim_model'] = Path("/zfsauton/project/public/ichar/models/bisimulation/halfcheetah")  # NOQA
+HALFCHEETAH_CONFIGS['halfcheetah-mixed']['use_bisimulation'] = False
+HALFCHEETAH_CONFIGS['halfcheetah-mixed']['penalize_stitches'] = False
+HALFCHEETAH_CONFIGS['halfcheetah-mixed']['bc_every_iter'] = False
 HALFCHEETAH_CONFIGS['halfcheetah-mixed']['epsilon_neighbors'] = 0.25
 
 HALFCHEETAH_CONFIGS['halfcheetah-medium'] = deepcopy(base_config)
@@ -47,7 +46,8 @@ for k, v in HALFCHEETAH_CONFIGS.items():
     config = deepcopy(v)
     config['use_all_planning_itrs'] = True
     config['continue_after_no_advantage'] = True
-    config['num_stitching_iters'] = 25
+    config['num_stitching_iters'] = 20
+    config['stitching_chunk_size'] = 5000
     # For mixed dataset edge distance = 4.48265 +- 1.07
     # config['epsilon_neighbors'] = 1.3
     config['bc_every_iter'] = False
