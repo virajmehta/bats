80a74c4a0804d9cb301cba38209b1845eb7c77d5
Viraj Mehta <thebigmehtaphor@gmail.com>
2021-05-18 16:53:40 -0700

    added configs for everythng

--- configs/__init__.py
+++ configs/__init__.py
@@ -4,9 +4,13 @@ from configs.halfcheetah import HALFCHEETAH_CONFIGS
 from configs.hopper import HOPPER_CONFIGS
 from configs.mazes import MAZE_CONFIGS
 from configs.walker import WALKER_CONFIGS
+from configs.mountaincar import MOUNTAINCAR_CONFIGS
+from configs.pendulum import PENDULUM_CONFIGS
 
 CONFIGS = OrderedDict()
 CONFIGS.update(HALFCHEETAH_CONFIGS)
 CONFIGS.update(HOPPER_CONFIGS)
 CONFIGS.update(MAZE_CONFIGS)
 CONFIGS.update(WALKER_CONFIGS)
+CONFIGS.update(PENDULUM_CONFIGS)
+CONFIGS.update(MOUNTAINCAR_CONFIGS)
