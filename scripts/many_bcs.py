8c15fbe5758bbf85150c02e1b223cfb349f384f8
Ian Char <ianchar4@gmail.com>
2021-05-22 13:00:54 -0400

    Many bc tweeks.

--- scripts/many_bcs.py
+++ scripts/many_bcs.py
@@ -7,7 +7,7 @@ from multiprocessing import Pool
 import os
 
 from scripts.boltclone_graph import run
-from util import s2f
+from util import s2f, s2i
 
 
 def launch_jobs(args):
@@ -18,16 +18,20 @@ def launch_jobs(args):
 
 def create_config_list(args):
     configs = []
+    devices = args.cuda_devices.split(',')
+    device_idx = 0
     for graph in os.listdir(args.target_dir):
         for t in range(1, args.trials + 1):
             config = deepcopy(args)
-            config['save_dir'] = os.path.join(
+            config.save_dir = os.path.join(
                     args.save_location,
                     graph + '_trial%d' % t,
             )
-            config['graph_dir'] = os.path.join(args.train_dir, graph)
-            config['silent'] = True
+            config.graph_dir = os.path.join(args.target_dir, graph)
+            config.silent = True
+            config.cuda_device = str(devices[device_idx])
             configs.append(config)
+            device_idx = (device_idx + 1) % len(devices)
     return configs
 
 
@@ -38,14 +42,14 @@ def parse_args():
     parser.add_argument('--save_location')
     parser.add_argument('--trials', type=int, default=3)
     parser.add_argument('--num_jobs', type=int, default=4)
-    parser.add_argument('--epochs', type=int, default=100)
+    parser.add_argument('--epochs', type=int, default=50)
     parser.add_argument('--batch_updates_per_epoch', type=int)
-    parser.add_argument('--od_wait', type=int, default=10)
+    parser.add_argument('--od_wait', type=int, default=25)
     # If None, then collect as many points as there are in the dataset.
-    parser.add_argument('--n_collects', type=int, default=None)
+    parser.add_argument('--n_collects', type=int, default=1000000)
     parser.add_argument('--n_val_collects', type=int, default=0)
     parser.add_argument('--val_start_prop', type=float, default=0)
-    parser.add_argument('--val_selection_prob', type=float, default=0.1)
+    parser.add_argument('--val_selection_prob', type=float, default=0.2)
     parser.add_argument('--temperature', type=float, default=0)
     parser.add_argument('--pi_architecture', default='256,256')
     parser.add_argument('--real_edges_only', action='store_true')
@@ -60,9 +64,11 @@ def parse_args():
     parser.add_argument('--use_graphs_starts', action='store_true')
     parser.add_argument('--target_entropy', type=float, default=None)
     parser.add_argument('--all_starts_once', action='store_true')
+    parser.add_argument('--planning_quantile', type=float)
+    parser.add_argument('--epsilon_planning', type=float)
     parser.add_argument('--stitch_itr', type=int)
-    parser.add_argument('--cuda_device', type=str, default='')
-    parser.add_argument('--graph_name', default='trimmed.gt')
+    parser.add_argument('--cuda_devices', type=str, required=True)
+    parser.add_argument('--graph_name', default='vi.gt')
     parser.add_argument('--fusion', action='store_true')
     parser.add_argument('--max_path_length', type=int)
     parser.add_argument('--pudb', action='store_true')
